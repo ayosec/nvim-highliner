43de1b61ebd941910051916c3dd6798761f12a28
Ayose <ayosec@gmail.com>
2026-05-16 00:00:00 +0000

    Update flake.

--- flake.nix
+++ flake.nix
@@ -4,51 +4,30 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    flake-utils.url =
-      "github:numtide/flake-utils/1ef2e671c3b0c19053962c07dbda38332dcebf26";
+    flake-utils.url = "github:numtide/flake-utils/1ef2e671c3b0c19053962c07dbda38332dcebf26";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.writeShellScriptBin "fmt" ''
+        exec ${pkgs.alejandra}/bin/alejandra -q "$@";
+      '';
 
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        make-all = pkgs.writeShellScriptBin "make-all" ''
-          if [ $# -eq 0 ]
-          then
-              set -- make test lint fmt
-          fi
-
-          if "$@"
-          then
-              c=42
-          else
-              c=41
-          fi
-
-          printf '\e[%dm\e[K\e[m\n' $c
-          test $c -ne 41
-        '';
-
-        watch = pkgs.writeShellScriptBin "watch" ''
-          exec ${pkgs.watchexec}/bin/watchexec --restart --quiet -n \
-            -- ${make-all}/bin/make-all "$@"
-        '';
-
-      in {
-        formatter = pkgs.nixfmt;
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            gnumake
-            lua-language-server
-            luajitPackages.luacheck
-            ripgrep
-            stylua
-            watch
-            watchexec
-          ];
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gnumake
+          lua-language-server
+          luajitPackages.luacheck
+          ripgrep
+          stylua
+          watchexec
+        ];
+      };
+    });
 }
