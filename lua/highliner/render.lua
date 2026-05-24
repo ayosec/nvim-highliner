0742d95414a05a9b4325091714cfa3ac8d60f971
Ayose <ayosec@gmail.com>
2026-05-24 00:00:00 +0000

    Add all extmarks in on_win callback.
    Since extmarks are still used even when `on_win` returns false, we
    don't need to add them line by line.
    
    The performance improvement should be barely noticeable, but the
    implementation is much simpler.

--- lua/highliner/render.lua
+++ lua/highliner/render.lua
@@ -3,42 +3,36 @@ local M = {}
 local BufState = require("highliner.bufstate")
 local Logger = require("highliner.logger")
 
-local NAMESPACE = vim.api.nvim_create_namespace("Highliner")
+local NAMESPACE = vim.api.nvim_create_namespace("Highliner/Render")
 
---- @param buffer_lines { [integer]: vim.api.keyset.set_extmark }
+--- @param bufnr integer
 --- @param hl_name string
 --- @param node TSNode
 --- @param toprow integer
 --- @param botrow integer
-local function set_buffer_lines(buffer_lines, hl_name, node, toprow, botrow)
+local function set_buffer_lines(bufnr, hl_name, node, toprow, botrow)
     local row1, _, row2, col2 = node:range()
 
-    --- @type vim.api.keyset.set_extmark
-    local opts = {
+    -- In some parsers (like Markdown), blocks ends in the column 0 of the
+    -- next row. In those cases, the extmark ends one line above the end.
+    local row2_offset = col2 == 0 and 0 or 1
+
+    vim.api.nvim_buf_set_extmark(bufnr, NAMESPACE, math.max(row1, toprow), 0, {
         ephemeral = true,
         hl_group = hl_name,
         hl_eol = true,
         end_col = 0,
+        end_row = math.min(row2 + row2_offset, botrow),
         priority = 0,
-    }
-
-    for row = math.max(row1, toprow), math.min(row2, botrow) do
-        if row == row2 and col2 == 0 then
-            -- Don't add the last line if it ends at column 0.
-            break
-        end
-
-        buffer_lines[row] = opts
-    end
+    })
 end
 
 -- Implementation of the decoration provider events.
 
---- @param state highliner.render.DecorationState
 --- @param bufnr integer
 --- @param toprow integer
 --- @param botrow integer
-local function dc_win(state, bufnr, toprow, botrow)
+local function dc_win(bufnr, toprow, botrow)
     local bufstate = BufState.from_buffer(bufnr)
 
     if not bufstate then
@@ -48,14 +42,11 @@ local function dc_win(state, bufnr, toprow, botrow)
     local lang = bufstate.lang
     local tstree = bufstate.ts_parser:parse()[1]
 
-    local buffer_lines = {}
-    state.buffers[bufnr] = buffer_lines
-
     -- Apply highlights from tree-sitter queries.
     for _, query in pairs(lang.ts_queries) do
         for id, node, _ in query:iter_captures(tstree:root(), bufnr, toprow, botrow) do
             local name = query.captures[id]
-            set_buffer_lines(buffer_lines, name, node, toprow, botrow)
+            set_buffer_lines(bufnr, name, node, toprow, botrow)
         end
     end
 
@@ -65,7 +56,7 @@ local function dc_win(state, bufnr, toprow, botrow)
         for id, node, _ in hlq:iter_captures(tstree:root(), bufnr, toprow, botrow) do
             local target_group = lang.hl_groups[id]
             if target_group then
-                set_buffer_lines(buffer_lines, target_group, node, toprow, botrow)
+                set_buffer_lines(bufnr, target_group, node, toprow, botrow)
             end
         end
     end
@@ -73,41 +64,13 @@ local function dc_win(state, bufnr, toprow, botrow)
     return true
 end
 
---- @param state highliner.render.DecorationState
---- @param bufnr integer
---- @param row integer
-local function dc_line(state, bufnr, row)
-    local buffer = state.buffers[bufnr]
-    local opts = buffer and buffer[row]
-    if opts then
-        opts.end_row = row + 1
-        vim.api.nvim_buf_set_extmark(bufnr, NAMESPACE, row, 0, opts)
-    end
-end
-
 function M.setup()
-    --- @class (private) highliner.render.DecorationState
-    local state = {
-        --- @type { [integer]: { [integer]: vim.api.keyset.set_extmark } }
-        buffers = {},
-    }
-
     vim.api.nvim_set_decoration_provider(NAMESPACE, {
         on_win = function(_, _, bufnr, toprow, botrow)
-            return Logger.try(function()
-                return dc_win(state, bufnr, toprow, botrow)
-            end)
-        end,
-
-        on_line = function(_, _, bufnr, row)
             Logger.try(function()
-                dc_line(state, bufnr, row)
+                dc_win(bufnr, toprow, botrow)
             end)
-        end,
-
-        on_end = function(_, _)
-            -- Clear computed extmarks when the render is done.
-            state.buffers = {}
+            return false
         end,
     })
 
